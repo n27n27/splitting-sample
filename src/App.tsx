aef3f425244e662433eb4eb494c55ba43316cf83
n27 <skfdkfkalsrn@gmail.com>
2022-03-30 13:16:47 +0900

    코드 스플리팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, Suspense } from 'react';
 import './App.css';
+import logo from './logo.svg';
+import loadable from '@loadable/component';
+
+const SplitMe = loadable(() => import('./SplitMe'), {
+  fallback: <div>loading...</div>
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(!visible);
+  };
+
+  const onMouseOver = () => {
+    SplitMe.preload();
+  }
+
+  return(
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt="logo" />
+        <p onClick={onClick} onMouseOver={onMouseOver}>Hello React!</p>        
+          {visible && <SplitMe />}        
       </header>
     </div>
   );
